453e37f0ef0b129ad2732000ba94f7c14b5f26bb
Siro Díaz Palazón <sirodiaz93@gmail.com>
2018-04-26 01:21:04 +0200

    Added all methods in the template API

--- src/Siro/Klaviyo/KlaviyoTemplate.php
+++ src/Siro/Klaviyo/KlaviyoTemplate.php
@@ -9,7 +9,7 @@ class KlaviyoTemplate extends KlaviyoResponse
     private $apiKey;
     private $client;
 
-    public function __construct($client)
+    public function __construct($apiKey, $client)
     {
         $this->apiKey = $apiKey;
         $this->client = $client;
@@ -20,62 +20,9 @@ class KlaviyoTemplate extends KlaviyoResponse
      */
     public function getAll()
     {
-    }
-
-    /**
-     * POST /api/v1/email-templates
-     */
-    public function create()
-    {
-    }
-
-    /**
-     * PUT /api/v1/email-template/{{ TEMPLATE_ID }}
-     */
-    public function update()
-    {
-    }
-
-    /**
-     * DELETE /api/v1/email-template/{{ TEMPLATE_ID }}
-     */
-    public function delete()
-    {
-    }
-
-    /**
-     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/clone
-     */
-    public function clone()
-    {
-    }
-
-    /**
-     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/render
-     */
-    public function render()
-    {
-    }
-
-    /**
-     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/send
-     */
-    public function renderAndSend()
-    {
-    }
-
-
-    /**
-     * GET /api/v1/lists
-     */
-    public function getLists($type = null, $page = 0, $count = 20)
-    {
-        $response = $this->client->get("/api/v1/lists", [
+        $response = $this->client->get('/api/v1/email-templates', [
             'query' => [
-                'api_key' => $this->apiKey,
-                'type' => $type,
-                'page' => $page,
-                'count' => $count
+                'api_key' => $this->apiKey
             ]
         ]);
 
@@ -83,19 +30,107 @@ class KlaviyoTemplate extends KlaviyoResponse
     }
 
     /**
-     * POST /api/v1/lists
+     * POST /api/v1/email-templates
      */
-    public function createNew($name, $listType = 'standard')
+    public function create($name, $htmlTemplate)
     {
         $requestParams = [
             'form_params' => [
                 'api_key' => $this->apiKey,
                 'name' => $name,
-                'list_type' => $listType,
+                'html' => $htmlTemplate,
             ]
         ];
-        $response = $this->client->post("/api/v1/lists", $requestParams);
-        
+        $response = $this->client->post('/api/v1/email-templates', $requestParams);
+
         return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * PUT /api/v1/email-template/{{ TEMPLATE_ID }}
+     */
+    public function update($templateId, $name, $htmlTemplate)
+    {
+        $requestParams = [
+            'form_params' => [
+                'api_key' => $this->apiKey,
+                'name' => $name,
+                'html' => $htmlTemplate,
+            ]
+        ];
+        $response = $this->client->put("/api/v1/email-template/{$templateId}", $requestParams);
+
+        return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * DELETE /api/v1/email-template/{{ TEMPLATE_ID }}
+     */
+    public function delete($templateId)
+    {
+        $requestParams = [
+            'form_params' => [
+                'api_key' => $this->apiKey,
+            ]
+        ];
+        $response = $this->client->delete("/api/v1/email-template/{$templateId}", $requestParams);
+        return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/clone
+     */
+    public function clone($templateId, $newName)
+    {
+        $requestParams = [
+            'form_params' => [
+                'api_key' => $this->apiKey,
+                'name'    => $newName
+            ]
+        ];
+        $response = $this->client->post("/api/v1/email-template/{$templateId}/clone", $requestParams);
+        return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/render
+     */
+    public function render($templateId, array $context = [])
+    {
+        $requestParams = [
+            'form_params' => [
+                'api_key' => $this->apiKey,
+                'context'    => json_encode($context)
+            ]
+        ];
+        $response = $this->client->post("/api/v1/email-template/{$templateId}/render", $requestParams);
+        return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/send
+     */
+    public function renderAndSend($templateId, $fromEmail, $fromName, $subject, $to, array $context = [])
+    {
+        $requestParams = [
+            'form_params' => [
+                'api_key'    => $this->apiKey,
+                'from_email' => $fromEmail,
+                'from_name'  => $fromName,
+                'subject'    => $subject,
+                'to'         => $to,
+                'context'    => json_encode($context)
+            ]
+        ];
+        $response = $this->client->post("/api/v1/email-template/{$templateId}/send", $requestParams);
+        return $this->sendResponseAsObject($response);
+    }
+
+    /**
+     * POST /api/v1/email-template/{{ TEMPLATE_ID }}/send
+     */
+    public function send($templateId, $fromEmail, $fromName, $subject, $to, array $context = [])
+    {
+        return $this->renderAndSend($templateId, $fromEmail, $fromName, $subject, $to, $context);
     }
 }
